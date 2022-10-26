4aa0d9ce74c01ca2666f63a46e78adaeacd0331d
Jay Welsh <thealchemicalopus@gmail.com>
2022-10-26 18:06:12 +0200

    fix(useOTP): re-implement duplicate avoidance without infinite loop

--- src/hooks/useOTP.ts
+++ src/hooks/useOTP.ts
@@ -1,36 +1,39 @@
-import { useEffect, useState, useCallback } from "react";
+import { useState, useRef } from "react";
 
 const useOTP = () => {
   const [otp, setOTP] = useState<string>('000000');
   
-  const newOTP = useCallback(() => {
+  const newOTP = () => {
     const array = new Uint32Array(1);
     crypto.getRandomValues(array);
     let newOtpRaw = array[0];
     let freshOtp = newOtpRaw.toString().substring((newOtpRaw.toString().length - 6), (newOtpRaw.toString().length));
     // check that all of the numbers have changed, else regenerate until all numbers have changed
-    // let newOtpSplit = freshOtp.split('');
-    // let oldOtpSplit = otp.split('');
+    let newOtpSplit = freshOtp.split('');
+    let oldOtpSplit = otp.split('');
     let hasRepeat = false;
-    // let checkIndex = 0;
-    // for(let entry of newOtpSplit) {
-    //   console.log({checkIndex, oldOtpSplit, newOtpSplit});
-    //   if(entry === oldOtpSplit[checkIndex]) {
-    //     hasRepeat = true;
-    //     break;
-    //   }
-    //   checkIndex++;
-    // }
+    let checkIndex = 0;
+    for(let entry of newOtpSplit) {
+      console.log({checkIndex, oldOtpSplit, newOtpSplit});
+      if(entry === oldOtpSplit[checkIndex]) {
+        hasRepeat = true;
+        break;
+      }
+      checkIndex++;
+    }
     if(!hasRepeat) {
       setOTP(freshOtp);
     } else {
       newOTP();
     }
-  }, [])
+  }
 
-  useEffect(() => {
+  // Run newOTP on init of component
+  const initializedRef = useRef(false);
+  if (!initializedRef.current) {
+    initializedRef.current = true;
     newOTP();
-  }, [newOTP])
+  }
 
   return {
     otp,
